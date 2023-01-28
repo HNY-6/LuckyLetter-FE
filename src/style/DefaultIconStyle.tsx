7d5ab54717703c371d86f3f8ed5bcb9cf3072670
Kim Seong Hun <justfix010@gmail.com>
2023-01-29 01:45:31 +0900

    Style : 로그인 페이지 전역 스타일 추가

---
+++ src/style/DefaultIconStyle.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const DefaultIconStyle = styled.img`
+  position: absolute;
+  left: 13px;
+  top: 50%;
+  transform: translate(0%, -50%);
+  height: 24px;
+  width: 24px;
+`;
+export default DefaultIconStyle;
