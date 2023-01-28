c592d4d411b65b7851a7885dcfc6091b8811debb
Kim Seong Hun <justfix010@gmail.com>
2023-01-29 01:46:05 +0900

    Style : 로그인 페이지 스타일 구현

--- src/components/UI/WrapperBoxStyle.tsx
+++ src/components/UI/WrapperBoxStyle.tsx
@@ -2,11 +2,11 @@ import styled from "styled-components";
 import background from "../../assets/BG.png";
 
 const WrapperBoxStyle = styled.div`
-  position: relative;
-  padding: 54px 30px;
-  width: 390px;
+  max-width: 390px;
+  width: 100%;
   height: 844px;
   background-image: url(${background});
+  background-repeat: no-repeat;
   border-radius: 10px;
   box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.2);
   overflow: hidden;
