39a06e2ff9e2ab5248280d4182b9cc7bb2a9a65f
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-01-30 16:16:57 +0900

    Add: PageDescParagraph 컴포넌트 추가

---
+++ src/components/UI/PageDescParagraph.tsx
@@ -0,0 +1,11 @@
+import PageDescParagraphStyle from "./PageDescParagraphStyle";
+
+interface GlobalLayoutProps {
+  children?: React.ReactNode;
+}
+
+const PageDescParagraph = (props: GlobalLayoutProps) => {
+  return <PageDescParagraphStyle>{props.children}</PageDescParagraphStyle>;
+};
+
+export default PageDescParagraph;
