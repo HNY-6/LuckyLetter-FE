f4ae48ac815d9e5f09c15204829f8a319a9bc6cb
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-01-30 18:51:02 +0900

    Feat: TabButton 기능 추가

---
+++ src/components/ReadMyPocket/TabButtonItemStyle.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const TabButtonItemStyle = styled.li`
+  & button {
+    width: 100px;
+    padding: 12px 0;
+    font-weight: bold;
+    color: var(--bg-color);
+    border-radius: 3px;
+    background-color: #d9d9d9;
+  }
+
+  & button.active {
+    background-color: #e48254;
+  }
+`;
+
+export default TabButtonItemStyle;
---
+++ src/components/ReadMyPocket/TabButtonListStyle.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const TabButtonListStyle = styled.ul`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export default TabButtonListStyle;
