f072718df6c9f51738001a3993c3a4226802d512
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-01-30 23:12:57 +0900

    Add: mypocket 라우터 추가 및 Layout 코드 일부 수정

--- src/pages/ReadMyPocket.tsx
+++ src/pages/ReadMyPocket.tsx
@@ -1,17 +1,16 @@
 import TabButtonList from "@/components/ReadMyPocket/TabButtonList";
 import PageDescParagraph from "@/components/UI/PageDescParagraph";
-import WrapperBox from "@/components/UI/WrapperBox";
 
 const ReadMyPocket = () => {
   return (
-    <WrapperBox>
+    <>
       <PageDescParagraph>받은 복주머니</PageDescParagraph>
       {/* 탭 메뉴 컴포넌트 */}
       <TabButtonList />
       {/* 받은 복주머니 데이터 리스트 */}
 
       {/* 페이지네이션 버튼 */}
-    </WrapperBox>
+    </>
   );
 };
 
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
+import ReadMyPocket from "@/pages/ReadMyPocket";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='/login' element={<Login />}></Route>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/mypocket" element={<ReadMyPocket />}></Route>
     </Routes>
   );
 };
