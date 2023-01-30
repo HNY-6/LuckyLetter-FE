e33908e2d0fbfb6638d374739266c0a110b002a1
chuhoon <khchu1220@naver.com>
2023-01-30 22:49:19 +0900

    Style : 셀렉트 포켓 퍼블리싱 1차

---
+++ src/pages/SelectPocketPage.tsx
@@ -0,0 +1,64 @@
+import WrapperBox from "@/components/UI/WrapperBox";
+import styled from "styled-components";
+import bgSelectPocket from "@/assets/Select_Pocket_BG.png";
+import {
+  PocketOrangeImg,
+  PocketRedImg,
+  PocketBlueImg,
+  PocketGreenImg,
+  PocketYellowImg,
+} from "@/components/button/index.style";
+
+const SelectPocketWrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+  padding: 104px 30px 85px;
+`;
+
+const SelctPocketTitle = styled.h2`
+  font-family: "Pretendard";
+  font-weight: bold;
+  font-size: 25px;
+  line-height: 1.2;
+  text-align: center;
+  margin-bottom: 40px;
+`;
+
+const SelectPocketSpan = styled(SelctPocketTitle)`
+  display: inline;
+  color: var(--main-color);
+`;
+
+const SelectPocketBox = styled.div`
+  width: 330px;
+  height: 285px;
+  background-image: url(${bgSelectPocket});
+  padding: 48px 26.96px 77px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 16.97px;
+  place-items: center;
+  /* gap: 16.97px; */
+`;
+
+const SelectPocketPage = () => {
+  return (
+    <WrapperBox>
+      <SelectPocketWrapper>
+        <SelctPocketTitle>
+          <SelectPocketSpan>복주머니</SelectPocketSpan>와
+          <SelectPocketSpan> 세뱃돈</SelectPocketSpan>을<br /> 선택해주세요!
+        </SelctPocketTitle>
+        <SelectPocketBox>
+          <PocketRedImg></PocketRedImg>
+          <PocketBlueImg></PocketBlueImg>
+          <PocketGreenImg></PocketGreenImg>
+          <PocketYellowImg></PocketYellowImg>
+          <PocketOrangeImg></PocketOrangeImg>
+        </SelectPocketBox>
+      </SelectPocketWrapper>
+    </WrapperBox>
+  );
+};
+
+export default SelectPocketPage;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
+import SelectPocketPage from "@/pages/SelectPocketPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />}></Route>
       <Route path='/login' element={<Login />}></Route>
+      <Route path='/selectpocket' element={<SelectPocketPage />}></Route>
     </Routes>
   );
 };
