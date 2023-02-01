2c80e6bd268efbdd96e4f42972f4784c0d42b22f
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-02-01 14:23:05 +0900

    Feat: 받은 복주머니 Link 라우터 태그 추가

--- src/pages/ReadMyLetter.tsx
+++ src/pages/ReadMyLetter.tsx
@@ -1,15 +1,19 @@
+import { useLocation } from "react-router-dom";
+
 import PageDescParagraph from "@/components/UI/PageDescParagraph";
 import LetterContainer from "@/components/ReadMyLetter/LetterContainer";
 import LetterButton from "@/components/ReadMyLetter/LetterButton";
 
 const ReadMyLetter = () => {
+  const location = useLocation();
+
   return (
     <>
       <PageDescParagraph>
         <span>익명이</span>님이 보낸 복주머니
       </PageDescParagraph>
       {/* 편지지 */}
-      <LetterContainer />
+      <LetterContainer color={location.state.color} />
       {/* 삭제 / 확인 버튼 */}
       <LetterButton />
     </>
