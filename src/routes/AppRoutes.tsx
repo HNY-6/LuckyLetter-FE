9358ba8d79c2b08f0cdea2d58b9925ff8cab757b
Kim Seong Hun <justfix010@gmail.com>
2023-02-05 18:40:37 +0900

    Style : login route 수정

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,22 +1,24 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "@/pages/Home";
-import Login from "@/pages/Login";
-import ReadMyPocket from "@/pages/ReadMyPocket";
-import ReadMyLetter from "@/pages/ReadMyLetter";
-import SelectPocketPage from "@/pages/SelectPocketPage";
-import Join from "@/pages/Join";
-import WriteLetterPage from "@/pages/WriteLetterPage";
+import { Route, Routes } from 'react-router-dom';
+import Home from '@/pages/Home';
+import Join from '@/pages/Join';
+import Login from '@/pages/Login';
+import SelectLogin from '@/pages/SelectLogin';
+import ReadMyPocket from '@/pages/ReadMyPocket';
+import ReadMyLetter from '@/pages/ReadMyLetter';
+import SelectPocketPage from '@/pages/SelectPocketPage';
+import WriteLetterPage from '@/pages/WriteLetterPage';
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='/login' element={<Login />}></Route>
-      <Route path='/join' element={<Join />}></Route>
-      <Route path='/mypocket' element={<ReadMyPocket />}></Route>
-      <Route path='/mypocket/:myletterId' element={<ReadMyLetter />}></Route>
-      <Route path='/selectpocket' element={<SelectPocketPage />}></Route>
-      <Route path='/writeletter' element={<WriteLetterPage />}></Route>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/auth" element={<SelectLogin />}></Route>
+      <Route path="/auth/login" element={<Login />}></Route>
+      <Route path="/join" element={<Join />}></Route>
+      <Route path="/mypocket" element={<ReadMyPocket />}></Route>
+      <Route path="/mypocket/:myletterId" element={<ReadMyLetter />}></Route>
+      <Route path="/selectpocket" element={<SelectPocketPage />}></Route>
+      <Route path="/writeletter" element={<WriteLetterPage />}></Route>
     </Routes>
   );
 };
