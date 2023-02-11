2c5c3141e89dfb0e0a25359009c48503575a0c40
tada-js <justfix010@gmail.com>
2023-02-12 02:52:09 +0900

    Feat : react hook form 로그인 페이지

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import TitleLogo from '@/assets/common-title.png';
 import DefaultButton from '@/components/UI/Button';
 import { ValidationErrorTextStyle } from '@/components/UI/InputStyle';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 const MainCharacter = styled.div`
   display: flex;
@@ -22,29 +23,74 @@ const loginErrorMessageStyle = {
   margin: '20px 0px',
 };
 
+const disabledLoginBtnStyle = {
+  backgroundColor: '#D9D9D9',
+};
+
+interface FormInput {
+  userEmail: string;
+  userPassword: string;
+}
+
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isDirty },
+  } = useForm<FormInput>({ mode: 'onChange' });
+
+  const onSubmit: SubmitHandler<FormInput> = (data) => {
+    console.log(data);
+  };
+
   return (
     <>
       <MainCharacter>
         <TitleImg src={TitleLogo} />
       </MainCharacter>
-      <ValidationInput
-        label={'이메일'}
-        placeholder={'example@example.com'}
-        asterisk={false}
-      />
-      <ValidationInput
-        label={'비밀번호'}
-        placeholder={'********'}
-        asterisk={false}
-      />
-      <ValidationErrorTextStyle styleOverrides={loginErrorMessageStyle}>
-        {
-          '이메일 또는 비밀번호를 잘못 입력하셨습니다. 입력하신 내용을 다시 확인해 주세요.'
-        }
-      </ValidationErrorTextStyle>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <ValidationInput
+          register={register('userEmail', {
+            required: '이메일을 입력해 주세요.',
+            pattern: {
+              value: /^\S+@\S+$/i,
+              message: '이메일 형식이 올바르지 않습니다.',
+            },
+          })}
+          label={'이메일'}
+          placeholder={'example@example.com'}
+          asterisk={false}
+        />
+        <ValidationErrorTextStyle>
+          {errors.userEmail && errors.userEmail.message}
+        </ValidationErrorTextStyle>
 
-      <DefaultButton label={'로그인'} />
+        <ValidationInput
+          register={register('userPassword', {
+            required: '비밀번호를 입력해 주세요.',
+          })}
+          type={'password'}
+          label={'비밀번호'}
+          placeholder={'********'}
+          asterisk={false}
+        />
+        <ValidationErrorTextStyle>
+          {errors.userPassword && errors.userPassword.message}
+        </ValidationErrorTextStyle>
+        <ValidationErrorTextStyle styleOverrides={loginErrorMessageStyle}>
+          {/* {errors.userEmail || errors.userPassword
+            ? '이메일 또는 비밀번호를 잘못 입력하셨습니다. 입력하신 내용을 다시 확인해 주세요.'
+            : null} */}
+        </ValidationErrorTextStyle>
+        {!isDirty ? (
+          <DefaultButton
+            styleOverrides={disabledLoginBtnStyle}
+            label={'로그인'}
+          />
+        ) : (
+          <DefaultButton label={'로그인'} />
+        )}
+      </form>
     </>
   );
 };
